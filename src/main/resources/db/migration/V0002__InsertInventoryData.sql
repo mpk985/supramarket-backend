2c00c50acfc4d69ee3f7fbc38e26a6ba033cfd59
Michael Kelly <mpkelly1@uno.edu>
2019-08-23 19:27:41 -0700

    Update sql script with image asset

--- src/main/resources/db/migration/V0002__InsertInventoryData.sql
+++ src/main/resources/db/migration/V0002__InsertInventoryData.sql
@@ -1,3 +1,3 @@
 insert into car_inventory (year, make, model, trim, transmission, color, price, vin, car_sold, product_img)values 
-('1993', 'Toyota', 'Supra', 'SZ', 'AT', 'Silver', '$22,000', '6U900JZA800005040', 'true', 'src/main/resources/media/supra/5040/'),
+('1993', 'Toyota', 'Supra', 'SZ', 'AT', 'Silver', '$22,000', '6U900JZA800005040', 'false', 'html/assets/media/supra/5040/front.jpeg'),
 ('1997', 'Toyota', 'Supra', 'GZ', 'AT', 'Deep Jewel Green', '$75,000', '6U900JZA990009810', 'false', '/html/assets/media/supra/9810/green.jpg');
