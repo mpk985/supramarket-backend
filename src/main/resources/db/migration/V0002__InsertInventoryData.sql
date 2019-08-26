fd93a88bd861ed94f46799e99d4a84d6eafd667d
Michael Kelly <mpkelly1@uno.edu>
2019-08-25 22:55:55 -0700

    BE updates: Update sold inventory data script | cleared posts data script | deleted images table as unnecessary currently

--- src/main/resources/db/migration/V0002__InsertInventoryData.sql
+++ src/main/resources/db/migration/V0002__InsertInventoryData.sql
@@ -1,3 +1,5 @@
 insert into car_inventory (year, make, model, trim, transmission, color, price, vin, car_sold, product_img)values 
-('1993', 'Toyota', 'Supra', 'SZ', 'AT', 'Silver', '$22,000', '6U900JZA800005040', 'false', 'html/assets/media/supra/5040/front.jpeg'),
-('1997', 'Toyota', 'Supra', 'GZ', 'AT', 'Deep Jewel Green', '$75,000', '6U900JZA990009810', 'false', '/html/assets/media/supra/9810/green.jpg');
+('1993', 'Toyota', 'Supra', 'SZ', 'AT', 'Alpine Silver', '$22,000', '6U900JZA800005040', 'false', 'html/assets/media/supra/5040/front.jpeg'),
+('1997', 'Toyota', 'Supra', 'GZ', 'AT', 'Deep Jewel Green', '$75,000', '6U900JZA990009810', 'false', '/html/assets/media/supra/9810/green.jpg'),
+('1994', 'Toyota', 'Supra', 'SZ', 'AT', 'White', '', '6U900JZA9900000000', 'true', '/html/assets/media/supra/0000/white.jpeg'),
+('1995', 'Toyota', 'Supra', 'SZ', 'MT', 'Black', '', '6U900JZA9900000000', 'true', '/html/assets/media/supra/0000/black.jpeg');
