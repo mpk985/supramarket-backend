f4e052629c0c53867265c6e78e3fcc8ca44caa78
Michael Kelly <mpkelly1@uno.edu>
2022-04-10 10:52:01 -0700

    TWSTDLUCK: New API endpoitns, cleanup

--- src/main/java/com/thesupramarket/backend/controller/ProductControllerV1.java
+++ src/main/java/com/thesupramarket/backend/controller/ProductControllerV1.java
@@ -1,11 +1,10 @@
 package com.thesupramarket.backend.controller;
 
 
-import com.thesupramarket.backend.domain.Product;
 import com.thesupramarket.backend.service.ShopifyService;
 import com.thesupramarket.backend.view.ProductView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class ProductControllerV1 {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(ProductControllerV1.class);
+    public static final Logger LOGGER = LogManager.getLogger(ProductControllerV1.class);
 
     @Autowired
     private ShopifyService shopifyService;
--- src/main/java/com/thesupramarket/backend/service/ShopifyService.java
+++ src/main/java/com/thesupramarket/backend/service/ShopifyService.java
@@ -4,18 +4,19 @@ import com.thesupramarket.backend.domain.Product;
 import com.thesupramarket.backend.domain.ProductDTO;
 import com.thesupramarket.backend.domain.ProductList;
 import com.thesupramarket.backend.view.ProductView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.time.Instant;
+import java.util.*;
 
 import static com.thesupramarket.backend.converter.DomainToView.convertProductToView;
 import static com.thesupramarket.backend.converter.DomainToView.convertToCamelCase;
@@ -23,7 +24,7 @@ import static com.thesupramarket.backend.converter.DomainToView.convertToCamelCa
 @Service
 public class ShopifyService {
 
-    public static Logger LOGGER = LoggerFactory.getLogger(ShopifyService.class);
+    public static Logger LOGGER = LogManager.getLogger(ShopifyService.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -43,36 +44,55 @@ public class ShopifyService {
     @Value("${shopify.endpoint.product}")
     private String shopifyProductEndpoint;
 
+    private List<ProductView> allProducts;
+
+    private Instant lastProductRefresh = Instant.now();
+
+    private final long PRODUCT_REFRESH_INTERVAL_IN_MILLIS = 5000;
+
 
     /*  Query Shopify API for all Twisted Luck products      */
     public List<ProductView> getAllProducts() {
         Long start = System.currentTimeMillis();
-        String Url = "https://" + shopifyHostname + shopifyAllProductsEndpoint;
-        HttpEntity entity = createShopifyAuth();
+        Instant timeDifference = lastProductRefresh.minusMillis(PRODUCT_REFRESH_INTERVAL_IN_MILLIS);
+        boolean mustRefreshProductList = lastProductRefresh.compareTo(timeDifference) > 0 ? true : false;
 
-        LOGGER.info("Call to Get All Products from Shopify Store with URL: {}", Url);
-        ResponseEntity<ProductList> responseEntity = restTemplate.exchange(
-                Url,
-                HttpMethod.GET,
-                entity,
-                ProductList.class
-        );
+        if(Objects.isNull(allProducts) || CollectionUtils.isEmpty(allProducts) || mustRefreshProductList) {
 
-        List<Product> products = responseEntity.getBody().getProducts();
+            UriComponents url = UriComponentsBuilder.newInstance()
+                    .scheme("https").host(shopifyHostname).path(shopifyAllProductsEndpoint).build();
+            HttpEntity entity = createShopifyAuth();
 
-        LOGGER.debug(products.toString());
-        if(products == null){
-            products = Collections.emptyList();
-            LOGGER.info("");
+            LOGGER.info("Calling Shopify Store to GET All Products with URL: {}", url);
+            ResponseEntity<ProductList> responseEntity = restTemplate.exchange(
+                    url.toUriString(),
+                    HttpMethod.GET,
+                    entity,
+                    ProductList.class
+            );
+
+            List<Product> products = responseEntity.getBody().getProducts();
+
+            LOGGER.info(products.toString());
+            if (products == null) {
+                products = Collections.emptyList();
+                LOGGER.info("");
+            }
+            LOGGER.info("TTC getAllProducts: {}ms", System.currentTimeMillis() - start);
+
+            allProducts = convertToCamelCase(products);
+            return allProducts;
         }
-        LOGGER.info("TTC getAllProducts: {}ms", System.currentTimeMillis() - start);
-        return convertToCamelCase(products);
+        LOGGER.info("Products already populated.  TTC: {}ms", System.currentTimeMillis() - start);
+        return allProducts;
     }
 
     /*  Query Shopify API for an individual Product by its inventoryId       */
     public ProductView getProductById(Long id) {
         Long start = System.currentTimeMillis();
-        LOGGER.info("Call to Get All Products from Shopify Store");
+        ProductView pv = null;
+
+        LOGGER.info("Call Shopify Store for Product ID");
         String Url = "https://" + shopifyHostname + shopifyProductEndpoint + id + ".json";
         HttpEntity entity = createShopifyAuth();
         ResponseEntity<ProductDTO> responseEntity = restTemplate.exchange(
@@ -82,21 +102,29 @@ public class ShopifyService {
                 ProductDTO.class
         );
 
-        ProductView pv = convertProductToView(responseEntity.getBody().getProduct());
+        pv = convertProductToView(responseEntity.getBody().getProduct());
         LOGGER.info("TTC getProductById: {}ms", System.currentTimeMillis() - start);
         return pv;
     }
 
     /*  Randomly select a Product */
     public ProductView getRandomProduct() {
-        LOGGER.info("Call to Get a Random Product from Shopify Store");
-        ProductView pv = new ProductView();
-        List<ProductView> allProducts= getAllProducts();
-        if(!allProducts.isEmpty() && allProducts != null){
-            int numOfProducts = allProducts.size();
-            pv = allProducts.get(new Random().nextInt(numOfProducts));
+        Instant timeDifference = lastProductRefresh.minusMillis(PRODUCT_REFRESH_INTERVAL_IN_MILLIS);
+        boolean mustRefreshProductList = lastProductRefresh.compareTo(timeDifference) > 0 ? true : false;
+
+        if (Objects.isNull(allProducts) || CollectionUtils.isEmpty(allProducts) || mustRefreshProductList) {
+
+            LOGGER.info("Call to Get a Random Product from Shopify Store");
+            ProductView pv = new ProductView();
+            List<ProductView> allProducts = getAllProducts();
+            if (!allProducts.isEmpty() && allProducts != null) {
+                int numOfProducts = allProducts.size();
+                pv = allProducts.get(new Random().nextInt(numOfProducts));
+            }
+            return pv;
         }
-        return pv;
+        LOGGER.info("Products already available to randomize.");
+        return allProducts.get(new Random().nextInt(allProducts.size()));
     }
     
     /*  Auth Headers for Shopify */
